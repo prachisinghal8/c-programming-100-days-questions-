1b1a6ea1a5580d5ed50cf69d4d54eab823b7f9f0
prachisinghal8 <psinghal0808@gmail.com>
2026-08-12 21:45:15 +0530

    Update day1q2.c

--- day1q2.c
+++ day1q2.c
@@ -16,7 +16,11 @@ int main()
    printf("sum=%f,sub=%f,mul=%f,div=%f",sum,sub,mul,div);
    return 0;
 }
-   
-   
+
+C:\Users\PRACHI SINGHAL\Downloads\c programming>day1q2
+Hello!Enter two numbers:67 76
+a=67.00 b=76.00
+sum=143.000000,sub=-9.000000,mul=5092.000000,div=0.881579
+C:\Users\PRACHI SINGHAL\Downloads\c programming>
 
    
