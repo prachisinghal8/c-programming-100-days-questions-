0e7c3239777c7eb455409d99c5c2dc79fe54c159
prachisinghal8 <psinghal0808@gmail.com>
2026-08-15 15:56:55 +0530

    Add files via upload

---
+++ day5q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+float p,r,t,si,ci;
+scanf("%f%f%f",&p,&r,&t);
+
+  si=p*r*t/100;
+  ci=p*pow((1+r/100),t)-p;
+  
+  printf("Simple interest=%.2f,compound interest=%.2f",si,ci);
+  return 0;
+}
