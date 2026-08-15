09cc01c797762f63fe28ee273cfc0fef8cbb4949
prachisinghal8 <psinghal0808@gmail.com>
2026-08-15 11:51:39 +0530

    Update day3q2.c

--- day3q2.c
+++ day3q2.c
@@ -13,3 +13,8 @@ b=temp;
 printf("After swap:%d %d",a,b);
 return 0;
 }
+
+C:\Users\PRACHI SINGHAL\Downloads\c programming>day3q2
+6 7
+After swap:7 6
+C:\Users\PRACHI SINGHAL\Downloads\c programming>
