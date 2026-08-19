df2453598e0887564e667532a380990dee3fb580
prachisinghal8 <psinghal0808@gmail.com>
2026-08-19 11:07:06 +0530

    Update day9q2.c

--- day9q2.c
+++ day9q2.c
@@ -1,3 +1,11 @@
+Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F
+
+
 #include <stdio.h>
 int main()
 {
@@ -23,4 +31,9 @@ int main()
 
 	return 0;
 }
+
+C:\Users\PRACHI SINGHAL\Downloads\c programming>p
+Enter a p67
+Grade D
+
 
