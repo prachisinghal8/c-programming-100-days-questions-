1d4573d8aa4af1e7db734cbbd12783b2f85087bf
prachisinghal8 <psinghal0808@gmail.com>
2026-08-19 11:05:39 +0530

    Add files via upload

---
+++ day9q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int p;
+
+	printf("Enter a p");
+	scanf("%d",&p);
+
+	if(p>=90 && p<=100){
+	printf("Grade A");}
+
+	else if(p>=80){
+	printf("Grade B");}
+
+	else if(p>=70){
+	printf("Grade C");}
+
+	else if(p>=60){
+	printf("Grade D");}
+
+	else{
+	printf("Grade E");}
+
+	return 0;
+}
+
