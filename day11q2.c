a8f8c89b472dc4f8ad25c1910ad2cd7e98279d55
prachisinghal8 <psinghal0808@gmail.com>
2026-08-23 09:24:06 +0530

    Update day11q2.c

--- day11q2.c
+++ day11q2.c
@@ -1,3 +1,6 @@
+//Write a program to find profit or loss percentage given cost price and selling price.
+
+
 #include <stdio.h>
 int main()
 {
@@ -20,3 +23,9 @@ int main()
   
 return 0;
 }
+
+
+C:\Users\PRACHI SINGHAL\Downloads\c programming>day11q2
+125 160
+loss 22%
+
