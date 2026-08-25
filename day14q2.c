340735355bd5290e07e2ebb8f44d4b1adf778e0b
prachisinghal8 <psinghal0808@gmail.com>
2026-08-25 23:23:00 +0530

    Add files via upload

---
+++ day14q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,sum=0;
+	
+	printf("enter n:");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++);{
+	sum+=(2*i);}
+	
+	printf("sum of first %d even numbers=%d\n",n,sum);
+	
+	return 0;
+	}
+	
