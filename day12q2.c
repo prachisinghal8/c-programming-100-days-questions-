ad4169b91b65b43038433227d8a61a3d91f4e3eb
prachisinghal8 <psinghal0808@gmail.com>
2026-08-25 22:28:14 +0530

    Add files via upload

---
+++ day12q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+  int units;
+  float bill;
+  
+  printf("Enter units:");
+  scanf("%d",&units);
+  
+  if(units<=100){
+  bill=units*5;}
+  
+  else if(units<=200){
+  bill= 100*5+((units-100)*7);}
+  
+  else if(units<=300){
+  bill= 100*5+100*7+((units-200)*10);}
+  
+  else{
+  bill= 100*5+100*7+100*10+((units-300)*12);}
+  
+  printf("Bill=%.0f",bill);
+  
+  
+  return 0;
+  
+}
+  
+
+  
